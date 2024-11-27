650b672ba4c89a946b4c13525757d089db48add4
hadeersalah7 <hadeermhmd70@gmail.com>
2024-11-27 09:48:12 +0200

    creating custom class and global class

---
+++ Tailwind-Portfolio/src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import aboutSvg from '../assets/about.svg';
+import SkillsTitle from './SkillsTitle';
+const About = () => {
+    return (
+        <div>About</div>
+    )
+}
+
+export default About
